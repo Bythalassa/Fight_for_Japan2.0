465c0ee9f7a094c7b8dad7be2a4cea09a8146f11
Daniel Dentone <143889531+Bythalassa@users.noreply.github.com>
2026-07-09 19:10:23 -0500

    cooking script

--- Assets/Scripts/DummyMovementDospuntoZero.cs
+++ Assets/Scripts/DummyMovementDospuntoZero.cs
@@ -18,12 +18,15 @@ public enum DummyState
 public class DummyMovementDospuntoZero : MonoBehaviour
 {
     private GameObject playerTarget;
+    private GameObject enemyTarget;
     private Animator anim;
     private Health Vida; 
 
     [Header("Movimiento general")]
     public float Maxspeed = 3f;
     public float radiusMovement = 3f; //(medir que chota con esto pq wtf)
+    public float AttackRadious = 2.5f;
+    public float PassiveAttackRadious = 5f; //relativamente muy lejos del jugador
     public float sideOffset = 20f; //same here
     private float side; // -1 izquierda / 1 derecha respecto al player
 
@@ -46,6 +49,7 @@ public class DummyMovementDospuntoZero : MonoBehaviour
     void Start()
     {
         playerTarget = GameObject.FindGameObjectWithTag("Player");
+        enemyTarget = GameObject.FindGameObjectWithTag("Enemy");
         anim = GetComponent<Animator>();
         Vida = GetComponent<Health>();
         isOnCamRange = GetComponent<EnemyCameraCheck>();
@@ -57,9 +61,10 @@ public class DummyMovementDospuntoZero : MonoBehaviour
     {
 
         //main updated position
-        Vector3 targetPos = playerTarget.transform.position;
+        Vector3 PlayerTargetPos = playerTarget.transform.position;
+        Vector3 EnemyTargetPos = enemyTarget.transform.position;
         Vector3 myPos = transform.position;
-        Vector3 direction = (targetPos - myPos).normalized;
+        Vector3 direction = (PlayerTargetPos - myPos).normalized;
 
         switch (state)
         {
@@ -71,7 +76,6 @@ public class DummyMovementDospuntoZero : MonoBehaviour
                     currentTime += Time.deltaTime;
                     if (currentTime >= MaxTime)
                     {
-                        //-> ejecutar algo
                         speed = 0f; // ya no c mueve
                         Maxspeed = speed;
                         anim.SetTrigger("Idle");
@@ -81,24 +85,55 @@ public class DummyMovementDospuntoZero : MonoBehaviour
                     else if (currentTime == 0 ) { state = DummyState.Approach; }
                 }
                 break;
-            case DummyState.WalkBehind:
+
+            case DummyState.Approach:
                 {
+                    //allEnemy aproach it 
+                    transform.position += direction * Maxspeed * Time.deltaTime;
+
+                    //  if myPos == attackRadious { state = DummyState.campeo; } }
+
+                    //falta recovery/ Cross RANGE // ATTACKING / DEAD
+
+                    /*condiciones extracurrriculares
+                     * Caminar detras del player
+                     * if despues de 1.5f segundos 
+                     * 
+                     * { if (!enemyTarget.transform.position = x.1.39366,z ) {  state = DummyState.WalkBehind; }
+                     * else  transform.position += direction * Maxspeed * Time.deltaTime; 
+                     * 
+                     * if (5 EnemyTargetPos == AttackRadiousPassive ) {  state = DummyState.ExtremeSway; }
+                     * lo mueve ligeramente lejos al player
+                     * if (3 EnemyTargetPos == AttackRadious ) { state = DummyState.WaitPhase;  }
+                     * 
+                     * 
+                     * 
+                    */
+
 
                 }
                 break;
-            case DummyState.Approach:
+            case DummyState.WalkBehind:
                 {
-
+                  //  if (enemyTarget.transform.position = x.1.39366,z ) { state = DummyState.Approach; }
                 }
                 break;
             case DummyState.WaitPhase:
                 {
+                    //movimiento normal de waIT PHASE lo mueve más cerca al player
+
+                    //if (!3 EnemyTargetPos == AttackRadious ) { state = DummyState.campeo; }
 
                 }
                 break;
             case DummyState.Camping:
                 {
-
+                    
+                    //que se mueva en campeo 
+                    //en direccion -> alejandose de player = camina
+                    //llega a posicion del player de vuelta = puede atacar = i (si i = 3 {case switch to attack y i= 0 })
+                    //If dentro de campeo is facing player 
+                    //if (3 EnemyTargetPos == AttackRadious ) { state = DummyState.WaitPhase; }
                 }
                 break;
             case DummyState.CrossRange:
@@ -118,7 +153,10 @@ public class DummyMovementDospuntoZero : MonoBehaviour
                 break;
             case DummyState.Attacking:
                 {
-
+                    //solo animAtaque -> y Do damage . take from Vida 
+                    //
+                    //if player moving in y =  case switch campeo 
+                    //if player moving in x =  case switch wait phase  
                 }
                 break;
             case DummyState.Dead:
