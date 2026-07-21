d2834934202a1991a3fa9fb227618b247c795285
Daniel Dentone <143889531+Bythalassa@users.noreply.github.com>
2026-07-21 15:05:11 -0500

    spawner is trigger conflict

--- Assets/Scripts/Definitiv/ArbustoHandle.cs
+++ Assets/Scripts/Definitiv/ArbustoHandle.cs
@@ -4,9 +4,11 @@ public class ArbustoHandle : MonoBehaviour
 {
     [Header("Spawner")]
     public SpawnerxTrigger enemySpawner;
+    public Health scriptHealth;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        scriptHealth.destroyOnDeath = true;
         if (collision.CompareTag("Player"))
         {
             Debug.Log("Player entered trigger.");
@@ -14,6 +16,7 @@ public class ArbustoHandle : MonoBehaviour
             if (enemySpawner != null)
             {
                 enemySpawner.SpawnFromTrigger();
+
             }
         }
     }
