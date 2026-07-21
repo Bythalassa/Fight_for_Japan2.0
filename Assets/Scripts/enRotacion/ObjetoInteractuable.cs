9a3adc4aa1bd87d429e10aed7e1203507e18fd8c
Daniel Dentone <143889531+Bythalassa@users.noreply.github.com>
2026-07-21 13:01:49 -0500

    llegaron los problemas

--- Assets/Scripts/enRotacion/ObjetoInteractuable.cs
+++ Assets/Scripts/enRotacion/ObjetoInteractuable.cs
@@ -2,15 +2,50 @@ using UnityEngine;
 
 public class ObjetoInteractuable : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public float timeToRespawn = 3.0f;
+
+    private float timeRespawn;
+    private bool interactuable = true;
+    private SpriteRenderer sprite;
+    private Health playerHealth;
+    public Health myHealth;
+
+    private void Start()
     {
-        
+        sprite = GetComponent<SpriteRenderer>();
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        playerHealth = Relevo.CurrentPlayer.GetComponent<Health>();
+
+
+        if (interactuable && myHealth.vida <=0 )
+        {
+            
+            if (playerHealth != null)
+            {
+                playerHealth.vida += 100;
+            }
+            sprite.enabled = false;
+            interactuable = false;
+            timeRespawn = 0f;
+        }
+
+        if (!interactuable)
+        {
+            timeRespawn += Time.deltaTime;
+
+            if (timeRespawn >= timeToRespawn)
+            {
+                interactuable = true;
+                sprite.enabled = true;
+            }
+            return;
+        }
+
     }
 }
